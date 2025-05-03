389a2175d7d4afbf91ea7be96ecfa1df0a319f8c
BadrChoubai <choubai.badr@protonmail.com>
2025-05-03 11:03:36 -0600

    Update application to take name argument

--- cmd/greeter/main.go
+++ cmd/greeter/main.go
@@ -1,28 +1,39 @@
-// Package main is our application's entrypoint
 package main
 
 import (
 	"context"
 	"fmt"
+	"io"
 	"os"
 	"os/signal"
 )
 
 func run(
 	ctx context.Context,
+	args []string,
+	stdout io.Writer,
 ) error {
-	_, cancel := signal.NotifyContext(ctx, os.Interrupt)
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
 	defer cancel()
 
-	fmt.Println("Hello, World!")
+	var name string
 
+	if len(args) > 1 {
+		name = args[1]
+	}
+
+	if name == "" {
+		return fmt.Errorf("name must not be empty")
+	}
+
+	fmt.Fprintf(stdout, "Hello, %s!", name)
 	return nil
 }
 
 func main() {
 	ctx := context.Background()
 
-	if err := run(ctx); err != nil {
+	if err := run(ctx, os.Args, os.Stdout); err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
 	}
